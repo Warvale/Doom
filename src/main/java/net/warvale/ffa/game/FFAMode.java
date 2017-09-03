2bf8f8335362bfa33a7645c1053c3a2cf33f1049
Eric <ehock7229@outlook.com>
2017-09-03 11:41:19 -0400

    update FFAMode to have table names

--- src/main/java/net/warvale/ffa/game/FFAMode.java
+++ src/main/java/net/warvale/ffa/game/FFAMode.java
@@ -2,11 +2,20 @@ package net.warvale.ffa.game;
 
 public enum FFAMode {
 
-    NONE,
-    BUILDUHC,
-    SOUP,
-    UHC,
-    SG,
-    NODEBUFF
+    NONE("none"),
+    BUILDUHC("builduhc_ffa_stats"),
+    SOUP("soup_ffa_stats"),
+    UHC("uhc_ffa_stats"),
+    SG("sg_ffa_stats"),
+    NODEBUFF("nodebuff_ffa_stats");
 
+    private String table;
+
+    FFAMode(String table) {
+        this.table = table;
+    }
+
+    public String getTable() {
+        return this.table;
+    }
 }
