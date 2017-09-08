0c96888ebfdbfa3f58ec0df67b2ce1512cd26e33
RONTheCookie <ronthecookie0101@gmail.com>
2017-09-08 20:00:06 +0300

    launchpads

---
+++ src/main/java/net/warvale/ffa/listeners/LaunchpadListener.java
@@ -0,0 +1,22 @@
+package net.warvale.ffa.listeners;
+
+import javafx.scene.layout.Priority;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.util.Vector;
+
+public class LaunchpadListener implements Listener {
+    @EventHandler(priority = EventPriority.LOW)
+    public void onMove(PlayerMoveEvent e) {
+        Block curr = e.getTo().getBlock();
+        Block below = curr.getRelative(BlockFace.DOWN);
+        if (below.getType().equals(Material.WOOL) && curr.getType().equals(Material.IRON_PLATE)) {
+            e.getPlayer().setVelocity(new Vector(e.getPlayer().getVelocity().getX(), 1.0D, e.getPlayer().getVelocity().getZ()));
+        }
+    }
+}
