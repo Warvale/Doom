71b80bb62e2fcc1fe16f468ee72cf9affbd40b41
RONTheCookie <ronthecookie0101@gmail.com>
2017-09-08 19:52:46 +0300

    We dont really need this right now, players dont get those permissions anyway.

--- src/main/java/net/warvale/ffa/listeners/CommandListener.java
+++ src/main/java/net/warvale/ffa/listeners/CommandListener.java
@@ -9,94 +9,94 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class CommandListener implements Listener {
 
-    @EventHandler (priority = EventPriority.HIGH)
-    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
-        Player player = event.getPlayer();
-
-        if (event.getMessage().startsWith("/me")) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
-        }
-
-        if (event.getMessage().startsWith("/gamemode")) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
-        }
-
-        if (event.getMessage().startsWith("/enchant")) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
-        }
-
-        if (event.getMessage().startsWith("/xp")) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
-        }
-
-        if (event.getMessage().startsWith("/setblock")) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
-        }
-
-        if (event.getMessage().startsWith("/spreadplayers")) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
-        }
-
-        if (event.getMessage().startsWith("/gamerule")) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
-        }
-
-        if (event.getMessage().startsWith("/testforblock")) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
-        }
-
-        if (event.getMessage().startsWith("/testfor")) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
-        }
-
-        if (event.getMessage().startsWith("/achievement")) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
-        }
-
-        if (event.getMessage().startsWith("/effect")) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
-        }
-
-        if (event.getMessage().startsWith("/give")) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
-        }
-
-        if (event.getMessage().startsWith("/summon")) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
-        }
-
-        if (event.getMessage().startsWith("/bukkit:")) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
-        }
-
-        if (event.getMessage().startsWith("/pl") && !event.getMessage().startsWith("/playsound")) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
-        }
-
-        if (event.getMessage().startsWith("/plugins")) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
-        }
-
-        if (event.getMessage().startsWith("/minecraft:")) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
-        }
-
-    }
+//    @EventHandler (priority = EventPriority.HIGH)
+//    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+//        Player player = event.getPlayer();
+//
+//        if (event.getMessage().startsWith("/me")) {
+//            event.setCancelled(true);
+//            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
+//        }
+//
+//        if (event.getMessage().startsWith("/gamemode")) {
+//            event.setCancelled(true);
+//            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
+//        }
+//
+//        if (event.getMessage().startsWith("/enchant")) {
+//            event.setCancelled(true);
+//            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
+//        }
+//
+//        if (event.getMessage().startsWith("/xp")) {
+//            event.setCancelled(true);
+//            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
+//        }
+//
+//        if (event.getMessage().startsWith("/setblock")) {
+//            event.setCancelled(true);
+//            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
+//        }
+//
+//        if (event.getMessage().startsWith("/spreadplayers")) {
+//            event.setCancelled(true);
+//            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
+//        }
+//
+//        if (event.getMessage().startsWith("/gamerule")) {
+//            event.setCancelled(true);
+//            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
+//        }
+//
+//        if (event.getMessage().startsWith("/testforblock")) {
+//            event.setCancelled(true);
+//            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
+//        }
+//
+//        if (event.getMessage().startsWith("/testfor")) {
+//            event.setCancelled(true);
+//            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
+//        }
+//
+//        if (event.getMessage().startsWith("/achievement")) {
+//            event.setCancelled(true);
+//            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
+//        }
+//
+//        if (event.getMessage().startsWith("/effect")) {
+//            event.setCancelled(true);
+//            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
+//        }
+//
+//        if (event.getMessage().startsWith("/give")) {
+//            event.setCancelled(true);
+//            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
+//        }
+//
+//        if (event.getMessage().startsWith("/summon")) {
+//            event.setCancelled(true);
+//            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
+//        }
+//
+//        if (event.getMessage().startsWith("/bukkit:")) {
+//            event.setCancelled(true);
+//            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
+//        }
+//
+//        if (event.getMessage().startsWith("/pl") && !event.getMessage().startsWith("/playsound")) {
+//            event.setCancelled(true);
+//            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
+//        }
+//
+//        if (event.getMessage().startsWith("/plugins")) {
+//            event.setCancelled(true);
+//            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
+//        }
+//
+//        if (event.getMessage().startsWith("/minecraft:")) {
+//            event.setCancelled(true);
+//            player.sendMessage(ChatColor.RED + "You do not have access to that command.");
+//        }
+//
+//    }
 }
