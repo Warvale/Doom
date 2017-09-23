346679f81fc3017d61fab729f9e5fbf7e64ce726
RONTheCookie <ronthecookie0101@gmail.com>
2017-09-23 16:31:22 +0300

    add the ultimate sadified kit (this is a joke??)

---
+++ src/main/java/net/warvale/ffa/kits/SadifiedKit.java
@@ -0,0 +1,28 @@
+package net.warvale.ffa.kits;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class SadifiedKit implements Kit{
+    private String name = "Sadified";
+    public String getName(){return this.name;}
+    public void giveKit(Player player){
+
+        PotionEffect pot=new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 1000000,0,true, false); // example potion
+        player.addPotionEffect(pot);
+        ItemStack helmet = new ItemStack(Material.DIAMOND_HELMET);
+        helmet.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
+        ItemStack[] armor = new ItemStack[4];
+        armor[1] = new ItemStack(Material.IRON_CHESTPLATE);
+        player.getInventory().setArmorContents(armor);
+        ItemStack enchbow = new ItemStack(Material.BOW);
+        enchbow.addEnchantment(Enchantment.ARROW_INFINITE, 1);
+        enchbow.addEnchantment(Enchantment.ARROW_DAMAGE, 2);
+        player.getInventory().setItem(0, enchbow);
+
+    }
+}
