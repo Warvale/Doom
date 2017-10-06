7ba2818d19652df5599b66d297e4ce4b70d82551
Henry <pixelificz@gmail.com>
2017-10-06 17:27:07 -0400

    Update UHCKit.java

--- src/main/java/net/warvale/ffa/kits/UHCKit.java
+++ src/main/java/net/warvale/ffa/kits/UHCKit.java
@@ -25,11 +25,12 @@ public class UHCKit implements Kit{
         armor[1] = new ItemStack(Material.IRON_LEGGINGS);
         armor[2] = new ItemStack(Material.IRON_CHESTPLATE);
         armor[3] = new ItemStack(Material.IRON_HELMET);
+        for (ItemStack stack : armor) {
+            stack.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1);
+        }
         player.getInventory().setArmorContents(armor);
         ItemStack sword = new ItemStack(Material.IRON_SWORD);
         player.getInventory().setItem(0, sword);
-        player.getInventory().setItem(1, new ItemStack(Material.BOW));
-        player.getInventory().setItem(7, new ItemStack(Material.ARROW, 4));
         player.getInventory().setItem(8, new ItemStack(Material.GOLDEN_APPLE, 2));
     }
 }
